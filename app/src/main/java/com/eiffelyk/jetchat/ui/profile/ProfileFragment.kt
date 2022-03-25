bc5eed228d6157719fbddb3bd1dbe76d4af3df87
馋猫 <eiffelyk@gmail.com>
2022-03-25 18:19:44 +0800

    ProfileFragment页面完成

--- app/src/main/java/com/eiffelyk/jetchat/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/eiffelyk/jetchat/ui/profile/ProfileFragment.kt
@@ -1,5 +1,49 @@
 package com.eiffelyk.jetchat.ui.profile
 
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import com.eiffelyk.jetchat.MainViewModel
+import com.eiffelyk.jetchat.ui.theme.JetchatTheme
 
-class ProfileFragment : Fragment()
+class ProfileFragment : Fragment() {
+    private val viewModel: ProfileViewModel by viewModels()
+    private val activityViewModel: MainViewModel by activityViewModels()
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        val userId = arguments?.getString("userId")
+        viewModel.setUserId(userId)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = ComposeView(inflater.context).apply {
+        layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        setContent {
+            val userData by viewModel.userData.observeAsState()
+            JetchatTheme {
+                if (userData == null) {
+                    ProfileError()
+                } else {
+                    ProfileScreen(userData = userData!!, onNavIconPressed = {
+                        activityViewModel.openDrawer()
+                    })
+                }
+            }
+        }
+    }
+}
